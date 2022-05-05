6550b845cf0765033f602d2706152251de34d917
crhistianchuqui <>
2022-05-05 16:26:01 +0000

    crhistianchuqui03

---
+++ EC/ejercicios31.java
@@ -0,0 +1,17 @@
+mport java.util.Scanner;
+
+public class ejercicio3 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int edad;
+        System.out.print("Ingresa el valor de edad: ");
+        edad = in.nextInt();
+        in.nextLine();
+        if(edad>=18)
+            System.out.println("Si puede votar en las proximas elecciones.");
+        else
+            System.out.println("No puede votar en las proximas elecciones.");
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# tarea-01-FP-CHUQUI
+#Declarar Variables
+edad : int
+mensaje : calle
+#Datos de Entrada
+edad = int ( input ( "Ingrese su edad:" ))
+#Proceso y Datos de Salida
+si  edad >= 18 :
+  mensaje = ( f"Puede votar porque su edad es { edad } " )
+más :
+  mensaje = ( f"No puede votar porque su edad es { edad } " )
+#Datos de Salida
+imprimir ( mensaje )
+© 2022 GitHub, Inc.
