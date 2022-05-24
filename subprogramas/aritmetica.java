41054b3914db9f804fdcfe50ff9e42816f20e856
crhistianchuqui03 <crhistian.chuquitarqui@upeu.edu.pe>
2022-05-24 14:47:22 +0000

    trabajo en clase

---
+++ subprogramas/aritmetica.java
@@ -0,0 +1,25 @@
+package subprogramas;
+
+/**
+ * *aritmetica
+ */
+public class aritmetica {
+    int va, vb;
+    
+    aritmetica(){
+    }
+    aritmetica(int va, int vb){
+        this.va=va;
+        this.vb=vb;
+    }
+
+
+
+    public int sumar(int a, int b) {
+        return a+b;
+    }
+    
+    public int sumar(int a, int b) {
+        return va+vb;
+    }
+}
---
+++ subprogramas/clasegeneral.java
@@ -0,0 +1,17 @@
+package subprogramas;
+
+import subprogramas.*;
+public class clasegeneral {
+    
+    public static void main(String[] args) {
+        aritmetica a=new aritmetica();
+        int resultado=a.sumar(5,3);
+        System.out.println(resultado);
+    
+        aritmeticaobj=new aritmetica(5,3);
+        resultados=obj.multiplicar();
+        System.out,println("multiplicacion: "+resultado);
+    }
+}
+
+ 
